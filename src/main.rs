6d1b84ba85dae731baaae3cacecc4fc664f3c610
Gisleno-bit <gisleno32+1@gmail.com>
2026-08-02 02:25:34 +0200

    Add Cargo.toml configuration for Rust project

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
+[package]
+name = "mvc-netmon"
+version = "0.1.0"
+edition = "2021"
 
+[dependencies]
+eframe = { version = "0.27", default-features = false, features = ["glow", "default_fonts"] }
+egui = "0.27"
+
+[profile.release]
+opt-level = 2
